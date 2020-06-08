e1986cb1dfb0661ded874c9574553367f70ae165
Sachin Arora <sachin.arora@sokrati.com>
2020-06-08 14:28:50 +0530

    Remove previous data and then add new data on get all data click button

--- ui.apps/src/main/content/jcr_root/apps/training/components/content/registration/clientlibs/js/registration.js
+++ ui.apps/src/main/content/jcr_root/apps/training/components/content/registration/clientlibs/js/registration.js
@@ -62,28 +62,66 @@ $('#submit').click(function() {
          url:'/bin/allRegistrationTrainingData',
          success: function(msg){
 
-             var trHTML = '';
-
-             if(msg.registrationData.length>0){
-                  var registrationDiv = document.getElementById("registration-div");
-			  registrationDiv.style.display="block";
-             }
-
-
+              var registrationDiv = document.getElementById("registration-div");
+              $('#registration-div').empty();
               $('#success-div').empty();
               $('#success-div').show();
               $('#success-div').append($('<div>').prop({innerHTML:msg.message }));
 			  $("#success-div").delay(1000).fadeOut(500);
 
+             if(msg.registrationData.length>0){
 
-               for(var i = 0; i < msg.registrationData.length; i++){
+			     registrationDiv.style.display="block";
+                 var table = document.createElement('table');
+                 table.style.cssText = 'width:100%';
+                 table.setAttribute('border', '1');
+                 var mainRow = document.createElement('tr');
 
-                    trHTML += '<tr><td>' + msg.registrationData[i].firstName + '</td><td>' + msg.registrationData[i].lastName + '</td><td>' + msg.registrationData[i].hobbies + '</td></tr>';
+                 var th1 = document.createElement('th');
+                 var th2 = document.createElement('th');
+                 var th3 = document.createElement('th');
 
-               }
+                 var firstNameHeading = document.createTextNode('First Name');
+                 var lastNameHeading = document.createTextNode('Last Name');
+                 var hobbiesHeading = document.createTextNode('Hobbies');
 
-               $('#registration-table').append(trHTML);
+                 th1.appendChild(firstNameHeading);
+                 th2.appendChild(lastNameHeading);
+                 th3.appendChild(hobbiesHeading);
 
+                 mainRow.appendChild(th1);
+                 mainRow.appendChild(th2);
+                 mainRow.appendChild(th3);
+
+                 table.appendChild(mainRow);
+
+                 for(var i = 0; i < msg.registrationData.length; i++){
+
+                     var tr = document.createElement('tr');
+
+                     var td1 = document.createElement('td');
+                     var td2 = document.createElement('td');
+                     var td3 = document.createElement('td');
+
+                     var text1 = document.createTextNode(msg.registrationData[i].firstName);
+                     var text2 = document.createTextNode(msg.registrationData[i].lastName);
+                     var text3 = document.createTextNode(msg.registrationData[i].hobbies);
+
+                     td1.appendChild(text1);
+                     td2.appendChild(text2);
+                     td3.appendChild(text3);
+
+                     tr.appendChild(td1);
+                     tr.appendChild(td2);
+                     tr.appendChild(td3);
+
+                     table.appendChild(tr);
+                }
+
+                 registrationDiv.appendChild(table);
+
+
+             }
          }
      });
     });
